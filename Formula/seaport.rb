0d4e5929c48a49a76975c3237d20f8ea5389c173
Haren S <harens@users.noreply.github.com>
2022-08-03 16:52:58 +0000

    seaport 0.6.1

--- Formula/seaport.rb
+++ Formula/seaport.rb
@@ -3,8 +3,8 @@ class Seaport < Formula
 
   desc "More mighty port bump for MacPorts"
   homepage "https://github.com/harens/seaport"
-  url "https://files.pythonhosted.org/packages/9d/95/afefc09993c0e4fa4d79bac54617ebe1f7f4173c56463a27d57c7b37d6c4/seaport-0.5.0.tar.gz"
-  sha256 "485b46626f0d8592683cf7892f9959181d50615ebc5f5587d4ed7b38a5cce97e"
+  url "https://files.pythonhosted.org/packages/37/b4/195e55af17d9e019262df8bab54e0b1506f7e1c56d899694a3b1727ec01c/seaport-0.6.1.tar.gz"
+  sha256 "d18350f07ab9466767e9d76e2d21275a56689a151768266dfae00a1de688b5ed"
   license "BSD-3-Clause"
   head "https://github.com/harens/seaport.git"
 
@@ -20,24 +20,36 @@ class Seaport < Formula
   end
 
   depends_on "gh"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   resource "beartype" do
-    url "https://files.pythonhosted.org/packages/c7/a8/7154f5dbf53553d244220a2c2736851dd22b60d85e59967a632795c17db6/beartype-0.6.0.tar.gz"
-    sha256 "f6167aaa2691db46669f1c2e2aa1f2c38f456fe8a669fe9d3f2af6ed97d083ec"
+    url "https://files.pythonhosted.org/packages/26/e1/8323460360d73ca09f24384483c9facbf9f586928bdd9f0e80fdcc72dace/beartype-0.10.4.tar.gz"
+    sha256 "24ec69f6a7f4e6e97af403d08de270def3248518060327095d23b1c4df64bf2a"
   end
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/27/6f/be940c8b1f1d69daceeb0032fee6c34d7bd70e3e649ccac0951500b4720e/click-7.1.2.tar.gz"
-    sha256 "d2b5255c7c6349bc1bd1e59e08cd12acbbd63ce649f2588755783aa94dfb6b1a"
+    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
+    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
   end
 
   def install
+    # Based on name-that-hash
+    # https://github.com/Homebrew/homebrew-core/blob/9652b75b2bbaf728f70c50b09cce39520c08321d/Formula/name-that-hash.rb
     virtualenv_install_with_resources
+
+    xy = Language::Python.major_minor_version Formula["python@3.10"].opt_bin/"python3"
+    site_packages = "lib/python#{xy}/site-packages"
+    pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
+    (prefix/site_packages/"homebrew-seaport.pth").write pth_contents
   end
 
   test do
     expected = "#{name}, version #{version}"
     assert_match expected, shell_output("#{bin}/#{name} --version")
+
+    system Formula["python@3.10"].opt_bin/"python3", "-c", <<~EOS
+      import #{name}
+      assert #{name}.__version__ == "#{version}"
+    EOS
   end
 end
